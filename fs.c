93efb00c5d00dce434b972ad12a7815d9722013b
srimugunthan <srimugunthan@srimugunthan-laptop.(none)>
2013-02-26 00:47:58 +0530

    fs.c filesystem specific things

---
+++ fs.c
@@ -0,0 +1,30 @@
+mkfs()
+{
+	/* create root directory entry
+ 	 * disk format of the filesystem
+         * _________________________________________________________
+	 * | bootblk | Superblk | inodes | alloc bitmap| Datablocks|
+	 * --------------------------------------------------------
+	 * initialise block allocation bitmap
+	 */
+}
+
+mountfs()
+{
+}
+
+unmountfs()
+{
+}
+
+
+fs_ls()
+{
+}
+
+fs_rm(char *name)
+{
+}
+
+
+
